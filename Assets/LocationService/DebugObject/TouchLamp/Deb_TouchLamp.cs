70ed83aba674b8184b6692e7b3a704f91cc0a1f1
yuutaro-O <yuutarousoft@gmail.com>
2017-08-21 23:03:44 +0900

    スポットをタップできるようにする
    ひとまずスポットがタップできるようになりました
    スポットをタップしたときの機能がいまいち不明なので何とも言えないですが、距離の計測自体はすでに可能なので、
    距離によって処理を変える事が出来るように改修します

---
+++ Assets/LocationService/DebugObject/TouchLamp/Deb_TouchLamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Deb_TouchLamp : MonoBehaviour {
+
+    Image thisImage;
+
+
+    // Use this for initialization
+    void Start () {
+        thisImage = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        thisImage.enabled = (Input.touchCount > 0) ? true : false;
+	}
+};
--- Assets/LocationService/Raycaster/CameraRaycaster.cs
+++ Assets/LocationService/Raycaster/CameraRaycaster.cs
@@ -7,7 +7,7 @@ public class CameraRaycaster : MonoBehaviour {
     Camera OriginCamera;
     [SerializeField]
     LayerMask TargetLayer;
-    Touch[] touches;
+    Renderer thisrenderer;  //デバッグ用、スポットヒット時に点灯するランプ
 	// Use this for initialization
 	void Start () {
 		if(OriginCamera == null)
@@ -15,24 +15,27 @@ public class CameraRaycaster : MonoBehaviour {
             Debug.Log("レイキャストを射出するカメラが指定されていません！\nMainCameraを取得し使用します");
             OriginCamera = Camera.main;
         }
+        thisrenderer = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        touches = Input.touches;
-        if(touches.Length > 0)
+        if (Input.touchCount > 0)
         {
-            Touch firstTouch = touches[0];
-            if (firstTouch.phase < TouchPhase.Ended)
+            //タップされている時に、スポット用のレイキャストを飛ばす
+            if (Input.GetTouch(0).phase < TouchPhase.Ended)
             {
                 RaycastHit hitinfo;
-                Physics.Raycast(OriginCamera.ScreenPointToRay(firstTouch.position), out hitinfo, 100, TargetLayer);
-                if((hitinfo.collider != null) && hitinfo.collider.CompareTag("Spot"))
+                if (Physics.Raycast(OriginCamera.ScreenPointToRay(Input.GetTouch(0).position), out hitinfo, Mathf.Infinity, TargetLayer) && hitinfo.transform.gameObject.CompareTag("Spot"))
                 {
-                    Debug.Log("Ray Hit");
+                    thisrenderer.enabled = true;
                     hitinfo.collider.gameObject.GetComponent<SpotClass>().SpotIntaract();
                 }
             }
+        }
+        else
+        {
+            thisrenderer.enabled = false;
         }
 	}
 }
