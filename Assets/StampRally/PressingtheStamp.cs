f4751d9f393bfe6f4e5580d1446a0361328e4b5a
Machida <aya14138@gmail.com>
2017-08-27 22:12:46 +0900

    【町田】 スタンプラリーの動き。 条件によって押される判定の認識。

---
+++ Assets/StampRally/PressingtheStamp.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class PressingtheStamp : MonoBehaviour {
+
+
+    [SerializeField]
+    Image TapMark;
+
+
+    public Image StampMark;
+    int count = 0;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        EventTrigger trigger = TapMark.GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+
+
+        //イベント検出
+        entry.eventID = EventTriggerType.PointerClick;
+        //コールバック登録
+        entry.callback.AddListener(Clicked);
+        //EventTriggerに追加
+        trigger.triggers.Add(entry);
+
+
+        StampMark = GameObject.Find("Canvas/StampMark").GetComponent<Image>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            count++;
+            Debug.Log(count);
+        }
+
+
+    }
+
+
+    void Clicked(BaseEventData eventData)
+    {
+
+
+        Debug.Log("(ﾟ∀ﾟ)");
+        
+
+        if(count == 5)
+        {
+
+            Debug.Log("(*ﾉωﾉ)");
+
+        }
+
+
+
+
+
+    }
+
+
+}
