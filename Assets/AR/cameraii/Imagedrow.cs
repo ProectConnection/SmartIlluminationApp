74265f488d980695e2ff42d04d2e9631f52d3f20
UmenaiT <umeta.gund@gmail.com>
2017-09-20 20:25:43 +0900

    AR

---
+++ Assets/AR/cameraii/Imagedrow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Imagedrow : MonoBehaviour {
+    public Transform[] image1=new Transform[10];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // Instantiate(image1, new Vector3(10, 10, 0), Quaternion.identity);
+    }
+}
