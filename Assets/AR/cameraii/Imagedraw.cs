e1925cf4f6062fbf54427273e9491287471248e4
UmenaiT <umeta.gund@gmail.com>
2017-10-02 12:51:49 +0900

    楳内:フォトフレームの生成処理

--- Assets/AR/cameraii/Imagedraw.cs
+++ Assets/AR/cameraii/Imagedraw.cs
@@ -4,43 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Imagedraw : MonoBehaviour {
-    [SerializeField]
-    Sprite[] tempphotoframe;
+    
     [SerializeField]
     Transform pealentTransform;
     
-    public GameObject[] img;
-    GameObject imgInstanceObject;
+    public GameObject img;
+    List<GameObject> imgInstanceObject;
+    
     //public GameObject SpotManagerGObject;
     // Use this for initialization
     void Start () {
+        imgInstanceObject = new List<GameObject>();
+        //nearestSpotoDataがnullかを確認する処理
         
-        //nearestSpotoDataがnullかを確認する処理を追加
-        // tempphotoframe = GameObject.FindGameObjectWithTag("SpotManager").GetComponent<SpotManager>().nearestSpotData.photoFrames;
         GameObject  SpotManagerGObject = GameObject.FindGameObjectWithTag("SpotManager");
-        
+
         if (SpotManagerGObject != null)
         {
-            Debug.Log("Test");
-            SpotData nearSpotdata = SpotManagerGObject.GetComponent<SpotManager>().nearestSpotData;
-            if (nearSpotdata != null) tempphotoframe = nearSpotdata.photoFrames;
-            foreach (Sprite frame in tempphotoframe)
-            {
-                //Imageのプレハブ生成
-                imgInstanceObject = Instantiate(img[0],pealentTransform);
-                imgInstanceObject.GetComponent<Image>().sprite = frame;
-            }
             
+            SpotData nearSpotdata = SpotManagerGObject.GetComponent<SpotManager>().nearestSpotData;
+            if (nearSpotdata != null)GeneratePhotoFrame(nearSpotdata.photoFrames);  
         }
         
 	}
-	
-	// Update is called once per frame
-	void Update () {
-       
-    }
-    public void Spots()
+
+    public void GeneratePhotoFrame(Sprite[] tempphotoframe)
     {
+        int i = 0;
+        foreach (GameObject tt in imgInstanceObject) {
+            Destroy(tt);
+        }
+        imgInstanceObject.Clear();
+         foreach (Sprite frame in tempphotoframe)
+        {
+            //Imageのプレハブ生成
+            imgInstanceObject.Add(Instantiate(img, pealentTransform));
+            imgInstanceObject[i].GetComponent<Image>().sprite = frame;
+            i++;
+        }
 
     }
 }
