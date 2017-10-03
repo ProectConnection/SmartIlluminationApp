d620ba3ee64f78452dcb227f5c4414a49ad76e34
Machida <aya14138@gmail.com>
2017-10-03 13:59:50 +0900

    【町田】マップ問題点修正

---
+++ Assets/FrontEnd/MapScene/MapDisplay.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour {
+
+
+    public GameObject Map;
+
+    public bool MapSwitch;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+        MapSwitch = false;
+
+        Map.SetActive(false);
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+
+		
+	}
+
+
+    public void SwitchDisplayMap()
+    {
+
+
+        if (MapSwitch == false)
+        {
+
+            Map.SetActive(true);
+            MapSwitch = true;
+
+        }else if(MapSwitch == true)
+        {
+
+            Map.SetActive(false);
+            MapSwitch = false;
+        }
+
+    }
+
+
+ 
+
+}
