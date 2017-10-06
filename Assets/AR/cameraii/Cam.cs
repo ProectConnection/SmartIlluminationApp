e6a4b28b88ccd4e24980d96f1172cd4992838a5f
UmenaiT <umeta.gund@gmail.com>
2017-10-06 10:04:54 +0900

    AR:インカメラ制作途中＆スクリーンショット時のカメラ取得問題修正

--- Assets/AR/cameraii/Cam.cs
+++ Assets/AR/cameraii/Cam.cs
@@ -24,38 +24,26 @@ public class Cam : MonoBehaviour {
     public  void OCamFlg() {
         //アウトカム
         WebCamDevice[] devices = WebCamTexture.devices;
+        int i = 0;
         WebCamTexture webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS);//0で外カメラ、1でインカメラ;
-        //デバイス名,映し出す幅,映し出す高さ,FPS値
-        if (incamera.incamflg == false) {
-            //text.text = "No";
-            webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS);//0で外カメラ、1でインカメラ
-            
-        }
-        else if (incamera.incamflg == true)
+                                                                                             //デバイス名,映し出す幅,映し出す高さ,FPS値
+        if (incamera.incamflg == true)
         {
-            //text.text ="ok";
-            webcamTexture = new WebCamTexture(devices[1].name, Width, Height, FPS);//0で外カメラ、1でインカメラ
+            for (i = 0; i < devices.Length; i++)
+            {
+                webcamTexture = new WebCamTexture(devices[1].name, Width, Height, FPS);//0で外カメラ、1でインカメラ
+                Debug.Log(i);
+            }
         }
-
-                                                                                          
+        else
+        {
+            i = 0;
+        }
         GetComponent<Renderer>().material.mainTexture = webcamTexture;   //オブジェクトのマテリアルを貼り付け
-
-        //実行
+                                                                         //実行
         webcamTexture.Play();
         Debug.Log("camera" + incamera.incamflg);
-        
     }
-  /* public  void ICamFlg() { 
-        //インカム
-        
-            WebCamTexture webcamTexture = new WebCamTexture(devices[1].name, Width, Height, FPS);//0で外カメラ、1でインカメラ
-            Debug.Log("camera" + incamera.incamflg);
-            //オブジェクトのマテリアルを貼り付け
-            GetComponent<Renderer>().material.mainTexture = webcamTexture;
-
-            //実行
-            webcamTexture.Play();
-        }*/
-   }
+}
 
     
