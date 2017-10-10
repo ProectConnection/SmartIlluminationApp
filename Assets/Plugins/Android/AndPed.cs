cb5f73ee6ddb82130d0871b3b94014b33a610ae7
unknown <nwuser@GM2150016.da.iwasaki.ac.jp>
2017-10-10 17:40:00 +0900

    AndroidPedmeterのプラグイン

---
+++ Assets/Plugins/Android/AndPed.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndPed : MonoBehaviour {
+
+    [SerializeField]
+    //private Text text;
+
+    public void callStaticFunction()
+    {
+        // AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        // using (AndroidJavaClass javaClass = new AndroidJavaClass("com.example.plugin.AndPlugin");
+        // 呼び込み
+        AndroidJavaObject plugin = new AndroidJavaObject("com.example.plugin");
+        //AndroidJavaObject plugin = new AndroidJavaObject("com.example.plugin.AndPlugin");
+        // 関数の呼び出し
+        plugin.CallStatic("staticFunction");
+    }
+
+    //public void onCallBackShowResult (string resultText);
+    //text.text = resultText;
+
+
+    /*
+        public class HogeClass {
+            private static string JAVA_CLASS_NAME = "com.hoge.JHogeClass";
+
+            public void Hoge() {
+                using (AndroidJavaClass plugin = new AndroidJavaClass(JAVA_CLASS_NAME)) {
+                    plugin.CallStatic("hogeNative");
+                }
+            }
+        }
+
+        //package com.hoge;
+
+        public class JHogeClass {
+            public static void hogeNative() {
+            }
+        }
+        */
+}
+
