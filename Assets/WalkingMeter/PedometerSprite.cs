b14cc5601311b56f966e37a7d1b208d4f6c2dad4
Machida <aya14138@gmail.com>
2017-10-13 11:41:33 +0900

    【町田】歩数計用プログラムの追加

--- Assets/WalkingMeter/PedometerSprite.cs
+++ Assets/WalkingMeter/PedometerSprite.cs
@@ -14,6 +14,24 @@ public class PedometerSprite : MonoBehaviour {
     public int Step_Achievement;
 
 
+    //************
+
+#if UNITY_IOS && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern void _ex_callSwiftMethod(string message);
+#endif
+
+    public static void CallSwiftMethod(string message)
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+        _ex_callSwiftMethod(message);
+#endif
+    }
+
+    //************
+
+
+
     // Use this for initialization
     void Start () {
        
