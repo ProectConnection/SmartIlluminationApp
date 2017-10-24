de1c25ed229d9d72363115197f29eb0c529c40cc
Machida <aya14138@gmail.com>
2017-10-25 03:09:08 +0900

    【町田】ARカメラメニュー表示

---
+++ Assets/AR/Script/Menu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+
+    public GameObject MenuDisplay;
+
+
+    bool MenuFlg;
+
+
+    // Use this for initialization
+    void Start () {
+
+        MenuDisplay.SetActive(false);
+        MenuFlg = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void OnMenu()
+    {
+        if (MenuFlg == false)
+        {
+            MenuDisplay.SetActive(true);
+            MenuFlg = true;
+
+        }
+        else if(MenuFlg == true)
+        {
+            MenuDisplay.SetActive(false);
+            MenuFlg = false;
+        }
+
+    }
+
+    public void OnFrame()
+    {
+
+
+
+    }
+
+
+}
