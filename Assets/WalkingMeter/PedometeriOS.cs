fca05535098d5be3106e1252b9011149561c20c2
yuutaro-O <yuutarousoft@gmail.com>
2017-12-01 11:49:26 +0900

    （大塚）：歩数計系のスクリプトの処理を整理
    ・PedometerSpriteに(歩数計シーン上で達成歩数のイメージを切り替えるスクリプト)歩数センサーの処理が入っていたのを
    └PedometeriOS、saecoPedometerに分離
    ・MapSceneシーンのオブジェクトにPedometerSpriteがアタッチされている問題を修正

---
+++ Assets/WalkingMeter/PedometeriOS.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedometeriOS : MonoBehaviour {
+    //************
+
+#if UNITY_IOS && !UNITY_EDITOR
+	[DllImport("__Internal")]
+	private static extern void _ex_callSwiftMethod(string CMPedmeter);
+#endif
+
+    public static void _ex_CallSwiftMethod(string CMPedmeter)
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+        _ex_callSwiftMethod(CMPedmeter);
+#endif
+    }
+
+}
