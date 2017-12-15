83ecdc12b18ed6caa32c10526a81942b5bad51a3
UmenaiT <umeta.gund@gmail.com>
2017-12-15 10:30:22 +0900

    楳内：撮った写真を確認できるミニウィンドウの実装。PCとAndroid実機での動作確認済、撮った写真がそのままミニウィンドウに表示されます。

--- Assets/AR/cameraii/Script/Thumbnails.cs
+++ Assets/AR/cameraii/Script/Thumbnails.cs
@@ -16,33 +16,40 @@ public class Thumbnails : MonoBehaviour {
         string DirectoryPass;
         RawImage rawImage = GetComponent<RawImage>();
         DateTime nowtime = DateTime.Now;
-        Debug.Log(Application.persistentDataPath + "/" + ("SmartIllumination_" + (nowtime.Year % 100).ToString("00") + nowtime.Month.ToString("00") + nowtime.Day.ToString("00")
+
+        //WindowsとiOSの撮った写真を左下のウィンドウに表示
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor|| Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            byte[] bytes = File.ReadAllBytes(Application.persistentDataPath + "/" + ("SmartIllumination_" + (nowtime.Year % 100).ToString("00") + nowtime.Month.ToString("00") + nowtime.Day.ToString("00")
             + nowtime.Hour.ToString("00") + nowtime.Minute.ToString("00") + nowtime.Second.ToString("00") + ".png"));
-        byte[] bytes = File.ReadAllBytes(Application.persistentDataPath + "/" + ("SmartIllumination_" + (nowtime.Year % 100).ToString("00") + nowtime.Month.ToString("00") + nowtime.Day.ToString("00")
-        + nowtime.Hour.ToString("00") + nowtime.Minute.ToString("00") + nowtime.Second.ToString("00") + ".png")); //"/1943[1].jpg"); 
-                                                                                                                  // byte[] bytes = File.ReadAllBytes(Application.persistentDataPath + "/1943[1].jpg");
 
+            Texture2D textures = new Texture2D(1, 1);
+            textures.filterMode = FilterMode.Trilinear;
+            textures.LoadImage(bytes);
 
+            rawImage.texture = textures;
+            rawImage.SetNativeSize();
+        } 
+         //byte[] bytes = File.ReadAllBytes(Application.persistentDataPath + "/1943[1].jpg");
+        
+        //Androidの撮った写真を左下のウィンドウに表示
         if (Application.platform == RuntimePlatform.Android)
         {
-            Debug.Log(_storageDir + "/DCIM/SmartIlluminationWalk" + "/" + ("SmartIllumination_" + (nowtime.Year % 100).ToString("00") + nowtime.Month.ToString("00") + nowtime.Day.ToString("00")
-            + nowtime.Hour.ToString("00") + nowtime.Minute.ToString("00") + nowtime.Second.ToString("00") + ".png"));
-
-            bytes = File.ReadAllBytes( "/DCIM/SmartIlluminationWalk" + "/" + ("SmartIllumination_" + (nowtime.Year % 100).ToString("00") + nowtime.Month.ToString("00") + nowtime.Day.ToString("00")
+           byte[] bytes = File.ReadAllBytes("/storage/emulated/0/DCIM/SmartIlluminationWalk" + "/" + ("SmartIllumination_" + (nowtime.Year % 100).ToString("00") + nowtime.Month.ToString("00") + nowtime.Day.ToString("00")
              + nowtime.Hour.ToString("00") + nowtime.Minute.ToString("00") + nowtime.Second.ToString("00") + ".png"));
-        }
+        
         Texture2D textures = new Texture2D(1, 1);
         textures.filterMode = FilterMode.Trilinear;
         textures.LoadImage(bytes);
        
         rawImage.texture = textures;
         rawImage.SetNativeSize();
-       
+        }
         RectTransform Rt = GetComponent<RectTransform>();
         Rt.sizeDelta = new Vector2(140, 184);
         //yield return null;
     }
-
+    
     // Update is called once per frame
 
 }
