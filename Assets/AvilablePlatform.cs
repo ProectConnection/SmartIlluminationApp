ecf9eb77540ec013036e963093c5283e02f0e264
yuutaro-O <yuutarousoft@gmail.com>
2017-12-20 00:52:40 +0900

    （大塚）：プラットフォームごとに使用可能なゲームオブジェクトを選定出来るようにするスクリプトを追加（AvilablePlatform.cs）
    このスクリプトをアタッチしたゲームオブジェクトは、Androidのみで使用可能なボタンや、
    iOSのみで使用可能なボタンなどといったように、プラットフォーム毎に使用可能にするかしないかを限定できるようになる。

---
+++ Assets/AvilablePlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvilablePlatform : MonoBehaviour {
+    //アタッチされているゲームオブジェクトが使用可能なプラットフォームのリスト
+    [SerializeField]
+    RuntimePlatform[] AvilablePlatforms;
+	// Use this for initialization
+	void Start () {
+		foreach(RuntimePlatform ap in AvilablePlatforms)
+        {
+            if (ap == Application.platform)
+            {
+                return;
+            }
+        }
+        gameObject.SetActive(false);
+	}
+
+}
