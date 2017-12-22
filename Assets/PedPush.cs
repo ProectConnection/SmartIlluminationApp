da5af0758e9c8ef47baf628343b9e2c731db75a4
Tateyama <nwuser@GM2150016.da.iwasaki.ac.jp>
2017-12-22 11:37:59 +0900

    申し訳ない 歩数計の通知機能

--- Assets/PedPush.cs
+++ Assets/PedPush.cs
@@ -3,45 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
     public class PedPush : MonoBehaviour
+{
+    [SerializeField]
+    List<NotiID> Pednoti = new List<NotiID>();
+
+    public List<NotiID> pednoti
     {
-    int Step;
+        get { return Pednoti; }
+        set { Pednoti = value; }
+    }
+
+    [SerializeField]
+    GameObject Pre_Notice;
+    [SerializeField]
+    Transform CanvasTransform;
+    [SerializeField]
+    Sprite deb_constructSprite;
+    NoticeManager ref_NoticeManager;
     DataSaver ref_DataSaver;
-    int[] FramePush = { 2000, 3000, 4000, 5000 }; 
-        
 
-        // Use this for initialization
-        void Start()
-        {
+    // 必要達成歩数
+    int[] Noticount = { 3000, 4000, 5000, 6000 };
 
+    // スプライトの登録
+    public Sprite notiSprite1, notiSprite2, notiSprite3, notiSprite4;
+
+    // Use this for initialization
+    void Start()
+    {
+        // DataSaverから歩数を取得
         ref_DataSaver = DataSaver.GetDataSaver();
+        StartCoroutine(LoadToDataSaver());
+
+        ref_NoticeManager = GameObject.Find("NotiManager").GetComponent<NoticeManager>();
+        
+    }
+
+    void Update()
+    {
         checkPedcount();
-
     }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        // テスト
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Step += 1000;
-            Debug.Log(Step);
-            checkPedcount();
-        }
+    public IEnumerator LoadToDataSaver()
+    {
+        DataSaver ref_dataSaver = DataSaver.GetDataSaver();
+        Pednoti = ref_dataSaver.Pednoti;
+        yield return null;
     }
+
+    public IEnumerator SaveToDataSaver()
+    {
+        DataSaver ref_dataSaver = DataSaver.GetDataSaver();
+        ref_dataSaver.SetDataPedNoti(pednoti);
+        yield return null;
+    }
+
 
     void checkPedcount()
     {
-        for (int i = 0; i < FramePush.Length; i++) {
-            // if (ref_DataSaver.Pedocount >= stepcount[i])
-            if (Step >= FramePush[i])
+
+        Debug.Log(ref_DataSaver.Pedocount+"aaaa");
+
+        while(ref_DataSaver.Pedocount >= Noticount[3])
+        {
+
+            if (ref_DataSaver.Pedocount >= Noticount[3])
             {
-                Debug.Log(FramePush[i] + "歩達成!");
+                Debug.Log(ref_DataSaver.Pedocount);
+                Debug.Log(Noticount[3] + "歩達成!");
+
+                ref_NoticeManager.CreateNotice(deb_constructSprite);
+                //pressedStamp.Add(neareSpotData.stampId);
+                StartCoroutine(SaveToDataSaver());
+
             }
+            else if (ref_DataSaver.Pedocount >= Noticount[2])
+            {
+                Debug.Log(ref_DataSaver.Pedocount);
+                Debug.Log(Noticount[2] + "歩達成!");
+
+            }
+            else if (ref_DataSaver.Pedocount >= Noticount[1])
+            {
+                Debug.Log(ref_DataSaver.Pedocount);
+                Debug.Log(Noticount[1] + "歩達成!");
+
+            }
+            else if (ref_DataSaver.Pedocount >= Noticount[0])
+            {
+                Debug.Log(ref_DataSaver.Pedocount);
+                Debug.Log(Noticount[0] + "歩達成!");
+
+            }
+           // ref_DataSaver.AddInitialPednoti(Noticount);
+            break;
         }
+
     }
-    
+
 }
