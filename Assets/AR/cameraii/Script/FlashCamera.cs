ff763a6fe01725a94e691cc39c82d0d566815e56
yuutaro-O <yuutarousoft@gmail.com>
2018-01-19 17:25:47 +0900

    （大塚）：(iOS)インカメラ使用時、フラッシュ機能が使用不可になるように

--- Assets/AR/cameraii/Script/FlashCamera.cs
+++ Assets/AR/cameraii/Script/FlashCamera.cs
@@ -2,20 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+using UnityEngine.UI;
 
 public class FlashCamera : MonoBehaviour {
-
-	// Use this for initialization
+    Button ClickButton;
+    [SerializeField]
+    Button inCameraButton;
+    // Use this for initialization
 #if UNITY_IOS && !UNITY_EDITOR
 	[DllImport("__Internal")]
 	//private static extern void _WriteImageToCamera(captureDevice);
 	private static extern void _WriteImageToCamera(string path,string CalledGameObjectName,string CalledMethodName);
 #endif
-	public void ToggleFlash(){
-		#if UNITY_IOS && !UNITY_EDITOR
-		_WriteImageToCamera(null,this.gameObject.name,null);
-		#endif
+    private void Start()
+    {
+        inCameraButton.onClick.AddListener(ToggleButtonActive);
+        ClickButton = GetComponent<Button>();
+    }
+
+    public void ToggleFlash(){
+        #if UNITY_IOS && !UNITY_EDITOR
+		        _WriteImageToCamera(null,this.gameObject.name,null);
+        #endif
+        
 	}
+
+    public void ToggleButtonActive()
+    {
+        ClickButton.enabled = !ClickButton.enabled;
+    }
 
 	
 
