7f2e199dc21cad408eae7c18f834251332043c4d
UmenaiT <umeta.gund@gmail.com>
2018-01-26 11:42:28 +0900

    楳内：タッチエフェクト　ボタンを離したときにエフェクト再生に変更確認よろしくお願いします

--- Assets/Tach/TapEffect.cs
+++ Assets/Tach/TapEffect.cs
@@ -29,7 +29,7 @@ public class TapEffect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
             ClickCecker = Input.mousePosition;
             ClickCecker.z = 5.0f;
