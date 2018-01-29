8dcceea55690839606c243968618052ce6fe4139
yuutaro-O <yuutarousoft@gmail.com>
2018-01-29 10:50:55 +0900

    （大塚）：Pednotiのマージコミット１
    ・前コミットで競合の為マージできなかったInitializeSceneの状態を手動マージ。
    ・PedPush.csの不要コードの削除。

--- Assets/PedPush.cs
+++ Assets/PedPush.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-    public class PedPush : MonoBehaviour
+public class PedPush : MonoBehaviour
 {
-    [SerializeField]
-    List<NotiID> Pednoti = new List<NotiID>();
+    //[SerializeField]
+    //List<NotiID> Pednoti = new List<NotiID>();
 
-    public List<NotiID> pednoti
-    {
-        get { return Pednoti; }
-        set { Pednoti = value; }
-    }
+    //public List<NotiID> pednoti
+    //{
+    //    get { return Pednoti; }
+    //    set { Pednoti = value; }
+    //}
 
     [SerializeField]
     Sprite deb_BGImage;
@@ -25,8 +25,8 @@ using UnityEngine.UI;
     int i = 0;
 
     // スプライトの登録
-    
-    public Sprite Pedmeter_chapter1, Pedmeter_chapter2, Pedmeter_chapter3, Pedmeter_chapter4;
+
+    //public Sprite Pedmeter_chapter1, Pedmeter_chapter2, Pedmeter_chapter3, Pedmeter_chapter4;
 
 
     // Use this for initialization
@@ -34,10 +34,10 @@ using UnityEngine.UI;
     {
         // DataSaverから歩数を取得
         ref_DataSaver = DataSaver.GetDataSaver();
-        StartCoroutine(LoadToDataSaver());
+        //StartCoroutine(LoadToDataSaver());
 
         ref_NoticeManager = GameObject.Find("NotiManager").GetComponent<NoticeManager>();
-        
+
     }
 
     void Update()
@@ -48,40 +48,33 @@ using UnityEngine.UI;
         }
     }
 
-    public IEnumerator LoadToDataSaver()
-    {
-        DataSaver ref_dataSaver = DataSaver.GetDataSaver();
-        Pednoti = ref_dataSaver.Pednoti;
-        yield return null;
-    }
+    //public IEnumerator LoadToDataSaver()
+    //{
+    //    DataSaver ref_dataSaver = DataSaver.GetDataSaver();
+    //    Pednoti = ref_dataSaver.Pednoti;
+    //    yield return null;
+    //}
 
-    public IEnumerator SaveToDataSaver()
-    {
-        DataSaver ref_dataSaver = DataSaver.GetDataSaver();
-        ref_dataSaver.SetDataPedNoti(pednoti);
-        yield return null;
-    }
+    //public IEnumerator SaveToDataSaver()
+    //{
+    //    DataSaver ref_dataSaver = DataSaver.GetDataSaver();
+    //    ref_dataSaver.SetDataPedNoti(pednoti);
+    //    yield return null;
+    //}
 
 
     void checkPedcount()
     {
-        
-        while (ref_DataSaver.Pedocount <= Noticount[3])
+        if (ref_DataSaver.Pedocount >= Noticount[i])
         {
 
-            if (ref_DataSaver.Pedocount >= Noticount[i])
-            {
-                
-                ref_NoticeManager.CreateNotice(deb_BGImage, Noticount[i]+"歩達成！");
-                //ref_NoticeManager.CreateNotice(deb_BGImage, "Pedmeter_chapter" + (i + 1));
-                StartCoroutine(SaveToDataSaver());
+            ref_NoticeManager.CreateNotice(deb_BGImage, Noticount[i] + "歩達成！");
+           
+            //StartCoroutine(SaveToDataSaver());
 
-                i++;
+            i++;
 
-            }
-            break;
         }
-       
     }
 
 }
